b4129f79d0428d882aef0897c57ef9332c3fe5d1
Sudhi Pulla <sudheendrakumar@gmail.com>
2014-10-25 12:53:38 +1300

    Added example for getProductById()

--- index.php
+++ index.php
@@ -3,6 +3,19 @@
 
 	$dal = new DAL();
 
+	echo '<h2>Get product by ID</h2>';
+	echo '<table>';
+	foreach ($dal->getProductById(1) as $product) {
+		echo '<tr>';
+		echo '<td>'.$product->getName().'</td>';
+		echo '<td>'.$product->getPrice().'</td>';
+		echo '</tr>';
+	}
+	echo '</table>';
+
+	echo '<hr/>';
+	echo '<h2>Get all products</h2>';
+
 	echo '<table>';
 	foreach ($dal->getProducts() as $product) {
 		echo '<tr>';
